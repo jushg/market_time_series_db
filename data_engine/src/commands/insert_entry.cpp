0c6224ec1edf8e66f7f4baa2e902a0e7cf28d161
Trong Tan <e0550425@u.nus.edu>
2023-11-14 15:38:35 +0800

    feat: add cmakelists and fix compile

--- data_engine/src/commands/insert_entry.cpp
+++
@@ -1,9 +0,0 @@
-#include "../../include/commands.hpp"
-
-
-void InsertEntryCommand::execute() {
-    std::vector<model::OrderData> orderToInsert{newEntry};
-    mergeStateAndWrite(config,newEntry.timestamp, newEntry.timestamp + 1, orderToInsert);
-}
-
-
